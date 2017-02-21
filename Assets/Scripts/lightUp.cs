d5fb868c5e04fd5d4397ad22378d6b95caf89303
Sebastian Gottschall <spikee.g@gmx.de>
2017-02-21 20:35:35 +0100

    ADD spheres and gamelogic

--- Assets/Scripts/lightUp.cs
+++ Assets/Scripts/lightUp.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class lightUp : MonoBehaviour {
-	/*public Material lightUpMaterial;
+public class lightUp : MonoBehaviour
+{
+    public Material lightUpMaterial;
 	public GameObject gameLogic;
 	private Material defaultMaterial;
 
 	// Use this for initialization
 	void Start () {
 		defaultMaterial = this.GetComponent<MeshRenderer> ().material; //Save our initial material as the default
-		this.GetComponentInChildren<ParticleSystem>().enableEmission = false; //Start without emitting particles
+		//this.GetComponentInChildren<ParticleSystem>().enableEmission = false; //Start without emitting particles
 
 		gameLogic = GameObject.Find ("gameLogic");
 	}
@@ -38,12 +40,12 @@ public class lightUp : MonoBehaviour {
 	}
 	public void aestheticReset() {
 		this.GetComponent<MeshRenderer>().material = defaultMaterial; //Revert to the default material
-		this.GetComponentInChildren<ParticleSystem>().enableEmission = false; //Turn off particle emission
-	}
+        //this.GetComponentInChildren<ParticleSystem>().enableEmission = false; //Turn off particle emission
+    }
 
 	public void patternLightUp() { //Lightup behavior when the pattern shows.
 		this.GetComponent<MeshRenderer>().material = lightUpMaterial; //Assign the hover material
-		this.GetComponentInChildren<ParticleSystem>().enableEmission = true; //Turn on particle emmission
+		//this.GetComponentInChildren<ParticleSystem>().enableEmission = true; //Turn on particle emmission
 		this.GetComponent<GvrAudioSource> ().Play (); //Play the audio attached
 	}
 
@@ -52,5 +54,9 @@ public class lightUp : MonoBehaviour {
 		patternLightUp ();
 		yield return new WaitForSeconds(duration-.1f);
 		aestheticReset ();
-	}*/
+	}
+    /*internal void patternLightUp(float puzzleSpeed)
+    {
+        throw new NotImplementedException();
+    }*/
 }
